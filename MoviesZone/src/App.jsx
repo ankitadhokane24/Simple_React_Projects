d43d1eeca67e7a81b78f870774fca1236c477fa3
ankitadhokane24 <ankitadhokane24@gmail.com>
2024-09-25 20:33:07 +0530

    first commit

---
+++ MoviesZone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Movies from './MovieZone/Movies'
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+        <Movies/>
+    </>
+  )
+}
+
+export default App
